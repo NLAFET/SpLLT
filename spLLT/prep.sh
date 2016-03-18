d3fb66e9b8c33fffec270716f4c95e994d30fed6
flopez <florent.lopez@stfc.ac.uk>
2016-03-18 14:43:45 +0000

    minor

--- spLLT/prep.sh
+++ spLLT/prep.sh
@@ -1,1 +1,11 @@
-/numerical/matrices/symmetric/UFL/prep.sh
+#!/bin/bash
+
+CACHE=/numerical/matrices/symmetric/UFL
+
+COLLECTION=`echo $1 | awk -F / '{ print $1 }'`
+PRBLM=`echo $1 | awk -F / '{ print $2 }'`
+
+rm matrix.rb
+tar -zxvpf $CACHE/$COLLECTION/$PRBLM.tar.gz > /dev/null
+cp $PRBLM/$PRBLM.rb matrix.rb
+rm -r $PRBLM
