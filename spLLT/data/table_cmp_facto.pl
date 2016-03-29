c6d6286c8165aecff4ce6fe62b755a352cd74d22
flopez <florent.lopez@stfc.ac.uk>
2016-03-29 17:29:54 +0100

    added tex file with results on scarf

--- spLLT/data/table_cmp_facto.pl
+++ spLLT/data/table_cmp_facto.pl
@@ -4,8 +4,8 @@ use ValueExtractor;
 use LatexPrint;
 
 # directory where runs are kept
-my $spllt_output_dir='spllt_starpu';
-my $ma87_output_dir='ma87';
+my $spllt_output_dir='cn255/spllt_starpu';
+my $ma87_output_dir='cn255/ma87';
 
 # my $facto_time_string = '[>] [spllt_stf_factorize] time:';
 my $spllt_facto_time_string = '\[>\] \[factorize\] time:';
@@ -15,7 +15,8 @@ my $ma87_facto_time_string  = 'Factor took';
 
 my $suffix = "_NCPU-27_NB-256";
 
-printf("%30s%12s%12s\n", " ", "spLLT", "MA87");
+printf("%-35s%12s%12s", " ", " & spLLT", "& MA87");
+printf(" \\\\\n");
 
 # Loop over problems input on stdin
 while(<>) {
@@ -32,11 +33,17 @@ while(<>) {
    $t_spllt = get_value("${spllt_output_dir}", $fprob, "_NCPU-27_NB-256", $spllt_facto_time_string, "mean");
    $t_ma87  = get_value("${ma87_output_dir}" , $fprob, "_NCPU-27_NB-256", $ma87_facto_time_string , "mean");
 
-   printf("%30s", $prob);
+   printf("%-35s", latex_escape($prob));
+   # printf("%30s", $prob);
+   # printf("%12.3e", $t_spllt);
+   printf(" & ");
    printf("%12.3e", $t_spllt);
+   # latex_float($t_spllt, 3);
+   printf(" & ");
    printf("%12.3e", $t_ma87 );
 
    # print "$t_spllt\n";
    # print "$t_ma87\n";
-   printf("\n");
+   # printf("\n");
+   printf(" \\\\\n");
 }
