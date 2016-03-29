06a86339bb6e6291802bea606fff3d31d8402d08
flopez <florent.lopez@stfc.ac.uk>
2016-03-29 15:18:37 +0100

    modified test script for testing more parameters

--- spLLT/run_tests.sh
+++ spLLT/run_tests.sh
@@ -18,8 +18,9 @@ outdir=data
 echo "[run_tests] build dir: $build_dir"
 #matrices=(JGD_Trefethen/Trefethen_20000)
 
-ncpu_list=(14 27)
-nb=256
+ncpu_list=(27)
+nb_list=(256)
+nemin_list=(8 16 32 64)
 trace_dir=/tmp
 prof_file=prof_file_scarf462_0
 # for matrix in ${matrices[@]}
@@ -40,20 +41,31 @@ do
     for ncpu in ${ncpu_list[@]}
     do
         echo "[run_tests] ncpu: $ncpu"
-        # ./run_ma87
-        echo "[run_tests] run MA87"
-        export OMP_NUM_THREADS=${ncpu} 
-        ./run_ma87 --ncpu ${ncpu} --nb ${nb} > $outdir/ma87/${matname}_NCPU-${ncpu}_NB-${nb}
-        echo "[run_tests] run SPLLT_STARPU"
-        rm -rf $trace_dir/$prof_file
-        # just to make sure
-        export OMP_NUM_THREADS=1
-        ./spllt_starpu_test --ncpu ${ncpu} --nb ${nb} > $outdir/spllt_starpu/${matname}_NCPU-${ncpu}_NB-${nb}
-        
-        if [ -f $trace_dir/$prof_file ];
-        then
-            mv $trace_dir/$prof_file $outdir/spllt_starpu/traces/${matname}_NCPU-${ncpu}_NB-${nb}.prof
-            starpu_fxt_tool -c -i $outdir/spllt_starpu/traces/${matname}_NCPU-${ncpu}_NB-${nb}.prof -o $outdir/spllt_starpu/traces/${matname}_NCPU-${ncpu}_NB-${nb}.trace
-        fi
+
+        for nb in ${nb_list[@]}
+        do
+            echo "[run_tests] nb: $nb"
+
+            for nemin in ${nemin_list[@]}
+            do  
+                echo "[run_tests] nemin: $nemin"
+
+                # ./run_ma87
+                echo "[run_tests] run MA87"
+                export OMP_NUM_THREADS=${ncpu} 
+                ./run_ma87 --ncpu ${ncpu} --nb ${nb} --nemin ${nemin} > $outdir/ma87/${matname}_NCPU-${ncpu}_NB-${nb}_NEMIN-${nemin}
+                echo "[run_tests] run SPLLT_STARPU"
+                rm -rf $trace_dir/$prof_file
+                # just to make sure
+                export OMP_NUM_THREADS=1
+                ./spllt_starpu_test --ncpu ${ncpu} --nb ${nb} --nemin ${nemin} > $outdir/spllt_starpu/${matname}_NCPU-${ncpu}_NB-${nb}_NEMIN-${nemin}
+                
+                if [ -f $trace_dir/$prof_file ];
+                then
+                    mv $trace_dir/$prof_file $outdir/spllt_starpu/traces/${matname}_NCPU-${ncpu}_NB-${nb}.prof
+                    starpu_fxt_tool -c -i $outdir/spllt_starpu/traces/${matname}_NCPU-${ncpu}_NB-${nb}.prof -o $outdir/spllt_starpu/traces/${matname}_NCPU-${ncpu}_NB-${nb}.trace
+                fi
+            done
+        done
     done
 done
