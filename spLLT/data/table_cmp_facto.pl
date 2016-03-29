a212b5b9cc5e0c1b21cbf191c58eee0730a4b3f4
flopez <florent.lopez@stfc.ac.uk>
2016-03-29 18:39:39 +0100

    minor

--- spLLT/data/table_cmp_facto.pl
+++ spLLT/data/table_cmp_facto.pl
@@ -2,6 +2,7 @@
 
 use ValueExtractor;
 use LatexPrint;
+use List::Util qw(min);
 
 # directory where runs are kept
 my $spllt_output_dir='cn255/spllt_starpu';
@@ -29,18 +30,45 @@ while(<>) {
 
    # print "$_\n";
    # print "$fprob\n";
+   my @t_spllt;
+   my @t_ma87;
    
-   $t_spllt = get_value("${spllt_output_dir}", $fprob, "_NCPU-27_NB-256", $spllt_facto_time_string, "mean");
-   $t_ma87  = get_value("${ma87_output_dir}" , $fprob, "_NCPU-27_NB-256", $ma87_facto_time_string , "mean");
+   # push @t_spllt, {'64' => get_value("${spllt_output_dir}", $fprob, "_NCPU-27_NB-64" , $spllt_facto_time_string, "mean")};
+   # push @t_spllt, {'128' => get_value("${spllt_output_dir}", $fprob, "_NCPU-27_NB-128", $spllt_facto_time_string, "mean")};
+   # push @t_spllt, {'256' => get_value("${spllt_output_dir}", $fprob, "_NCPU-27_NB-256", $spllt_facto_time_string, "mean")};
+   # push @t_spllt, {'512' => get_value("${spllt_output_dir}", $fprob, "_NCPU-27_NB-512", $spllt_facto_time_string, "mean")};
+
+   # push @t_ma87 , {'64' => get_value("${ma87_output_dir}" , $fprob, "_NCPU-28_NB-64" , $ma87_facto_time_string , "mean")};
+   # push @t_ma87 , {'128' => get_value("${ma87_output_dir}" , $fprob, "_NCPU-28_NB-128", $ma87_facto_time_string , "mean")};
+   # push @t_ma87 , {'256' => get_value("${ma87_output_dir}" , $fprob, "_NCPU-28_NB-256", $ma87_facto_time_string , "mean")};
+   # push @t_ma87 , {'512' => get_value("${ma87_output_dir}" , $fprob, "_NCPU-28_NB-512", $ma87_facto_time_string , "mean")};
+
+   # my $best_t_spllt = $t_spllt->{ ( sort {$a <=> $b} keys %$t_spllt )[0] };
+   # my $best_t_ma87  = $t_ma87->{ ( sort {$a <=> $b} keys %$t_ma87 )[0] };
+
+   push @t_spllt, get_value("${spllt_output_dir}", $fprob, "_NCPU-27_NB-64" , $spllt_facto_time_string, "mean");
+   push @t_spllt, get_value("${spllt_output_dir}", $fprob, "_NCPU-27_NB-128", $spllt_facto_time_string, "mean");
+   push @t_spllt, get_value("${spllt_output_dir}", $fprob, "_NCPU-27_NB-256", $spllt_facto_time_string, "mean");
+   push @t_spllt, get_value("${spllt_output_dir}", $fprob, "_NCPU-27_NB-512", $spllt_facto_time_string, "mean");
+
+   push @t_ma87 , get_value("${ma87_output_dir}" , $fprob, "_NCPU-28_NB-64" , $ma87_facto_time_string , "mean");
+   push @t_ma87 , get_value("${ma87_output_dir}" , $fprob, "_NCPU-28_NB-128", $ma87_facto_time_string , "mean");
+   push @t_ma87 , get_value("${ma87_output_dir}" , $fprob, "_NCPU-28_NB-256", $ma87_facto_time_string , "mean");
+   push @t_ma87 , get_value("${ma87_output_dir}" , $fprob, "_NCPU-28_NB-512", $ma87_facto_time_string , "mean");
+
+   my $best_t_spllt = min @t_spllt;
+   my $best_t_ma87  = min @t_ma87;
 
    printf("%-35s", latex_escape($prob));
    # printf("%30s", $prob);
    # printf("%12.3e", $t_spllt);
    printf(" & ");
-   printf("%12.3e", $t_spllt);
-   # latex_float($t_spllt, 3);
+   # printf("%12.3e", $t_spllt);
+   latex_float($best_t_spllt, 3, ($best_t_spllt<$best_t_ma87));
+
    printf(" & ");
-   printf("%12.3e", $t_ma87 );
+   # printf("%12.3e", $t_ma87 );
+   latex_float($best_t_ma87, 3, ($best_t_ma87<$best_t_spllt));
 
    # print "$t_spllt\n";
    # print "$t_ma87\n";
