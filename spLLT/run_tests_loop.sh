81c5cbd715cf1131586c48db0e29c0b95043195d
flopez <florent.lopez@stfc.ac.uk>
2016-04-07 12:37:58 +0100

    added test script

--- spLLT/run_tests_loop.sh
+++ spLLT/run_tests_loop.sh
@@ -1,55 +1,3 @@
-#!/bin/bash
-
-case $HOSTNAME in
-    gauss)
-        module purge
-        module load use.own
-        module load gnu/comp/default
-        module load gnu/mkl/seq/11.2.0
-        module load hwloc/1.11.2
-        module load fxt/0.3.1
-        module load starpu/trunk
-        module load metis/4.0.3
-        module load hsl/latest
-        module load spral/trunk
-        module list
-        ;;
-    cn202.scarf.rl.ac.uk | cn255.scarf.rl.ac.uk)
-        module purge
-        module load use.own
-        module load gcc/5.3.0
-        module load intel/mkl/11.3.1.150
-        module load hwloc/1.11.2
-        module load starpu/trunk-nogpu
-#        module load starpu/trunk-nogpu-nofxt
-        module load metis/4.0.3
-        module load hsl/latest
-        module load spral/trunk
-        module list
-        ;;
-esac
-
-build_dir=`pwd`
-id=`whoami`
-outdir=data
-#outsuffix="_NOSUB"
-outsuffix=
-
-echo "[run_tests] build dir: $build_dir"
-#matrices=(JGD_Trefethen/Trefethen_20000)
-
-ncpu_list=(27)
-nb_list=(768)
-nemin_list=(32)
-trace_dir=/tmp
-prof_file=prof_file_scarf462_0
-# for matrix in ${matrices[@]}
-
-mkdir -p $outdir
-mkdir -p $outdir/spllt_starpu
-mkdir -p $outdir/spllt_starpu/traces
-mkdir -p $outdir/ma87
-
 for matrix in `cat list.matrix`
 do
     matname=`echo $matrix | sed -e 's/\//_/g'`
@@ -71,9 +19,9 @@ do
                 echo "[run_tests] nemin: $nemin"
 
                 # ./run_ma87
-                # echo "[run_tests] run MA87"
-                # export OMP_NUM_THREADS=${ncpu} 
-                # ./run_ma87 --ncpu ${ncpu} --nb ${nb} --nemin ${nemin} > $outdir/ma87/${matname}_NCPU-${ncpu}_NB-${nb}_NEMIN-${nemin}
+                echo "[run_tests] run MA87"
+                export OMP_NUM_THREADS=${ncpu} 
+                ./run_ma87 --ncpu ${ncpu} --nb ${nb} --nemin ${nemin} > $outdir/ma87/${matname}_NCPU-${ncpu}_NB-${nb}_NEMIN-${nemin}
                 echo "[run_tests] run SPLLT_STARPU"
                 rm -rf $trace_dir/$prof_file
                 # just to make sure
