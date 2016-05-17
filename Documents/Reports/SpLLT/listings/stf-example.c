f33aea8150e7f9a99340cd257e4870a4759e07fd
flopez <florent.lopez@stfc.ac.uk>
2016-05-17 13:00:36 +0100

    progressed on report

--- Documents/Reports/SpLLT/listings/stf-example.c
+++ Documents/Reports/SpLLT/listings/stf-example.c
@@ -1,45 +1,4 @@
-/* Codelet definition for kernel f */
-struct starpu_codelet f_cl =
-  {
-    .where = STARPU_CPU,
-    .cpu_funcs = { f_cpu_func },
-    .nbuffers = 1
-  };
-
-/* Codelet definition for kernel g */
-struct starpu_codelet g_cl =
-  {
-    .where = STARPU_CPU | STARPU_CUDA,
-    .cpu_funcs = { g_cpu_func },
-    .cuda_funcs = { g_cuda_func },
-    .nbuffers = 3
-  };
-
-starpu_data_handle_t x_handle[N], y_handle[N];
-
-starpu_init();
-
-/* declaration of data handles */
-for (i = 0; i < N; i++) {  >\label{code:ex1}>
-  starpu_variable_data_register(&x_handle[i], STARPU_MAIN_RAM, (uintptr_t) &x[i], sizeof(double));
-  starpu_variable_data_register(&y_handle[i], STARPU_MAIN_RAM, (uintptr_t) &y[i], sizeof(double));
- }
-
-/* tasks submission */
-for (i = 1; i < N; i++) {  >\label{code:ex2}>
-
-  starpu_insert_task(&f_cl, 
-                     STARPU_RW, x_handle[i],
-                     0);
-
-  starpu_insert_task(&g_cl, 
-                     STARPU_R, x_handle[i],
-                     STARPU_R, y_handle[i-1],
-                     STARPU_W, y_handle[i],
-                     0);
-
+for (i = 1; i < N; i++) {
+   submit(f, x[i]:RW);
+   submit(g, x[i]:R, y[i-1]:R, y[i]:RW);
 }
-
-starpu_task_wait_for_all();  >\label{code:ex3}>
-
-starpu_shutdown();
