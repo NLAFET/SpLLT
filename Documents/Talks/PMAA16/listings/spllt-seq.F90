9b0d4a779c3c4ef3e5777b05850b1ccb0bd37333
flopez <florent.lopez@stfc.ac.uk>
2016-06-28 09:31:55 +0100

    added missing listing

---
+++ Documents/Talks/PMAA16/listings/spllt-seq.F90
@@ -0,0 +1,17 @@
+forall nodes snode in post-order
+   call alloc(snode) ! allocate data structures
+   
+   call init(snode) ! initianlize node structure
+end do
+
+forall nodes snode in post-order
+  ! factorize node
+   call factorize(snode) ! factorize block
+
+   ! update ancestor nodes
+   forall ancestors(snode) anode 
+     call update_btw(snode, anode)
+   end do
+
+  end do
+end do
---
+++ Documents/Talks/PMAA16/listings/spllt-task-seq.F90
@@ -0,0 +1,32 @@
+forall nodes snode in post-order
+   call alloc(snode) ! allocate data structures
+   
+   call init(snode) ! initianlize node structure
+end do
+
+forall nodes snode in post-order
+  ! factorize node
+  do k=1..n in snode
+    call factorize(blk(k,k)) ! factorize block
+    
+    do i=k+1..m in snode
+        call solve(blk(k,k), blk(i,k)) ! perform solve
+    end do
+
+    do j=k+1..n in snode
+       do i=k+1..m in snode
+          call update(blk(j,k), blk(i,k), blk(i,j))
+       end do
+    end do
+
+    ! update ancestor nodes
+    forall ancestors(snode) anode 
+      do j=k+1..p(anode) in snode
+         do i=k+1..m in snode
+            call update_btw(blk(j,k), blk(i,k), a_blk(rmap(i), cmap(j)))
+         end do
+      end do
+    end do
+
+  end do
+end do
