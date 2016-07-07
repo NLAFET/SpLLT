fd22dad122d96fb8e6b3394bd7a11b33ef02205e
JohnConnor <florent.joan.lopez@gmail.com>
2016-07-07 14:43:15 +0100

    added value extractor tool

---
+++ tools/vextractor.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+import re
+
+def get_value(f, pattern):
+    for line in f:
+        line = line.rstrip()
+        if re.search(pattern, line):
+            value = re.findall('[^0-9]*([0-9E.+-]*).*', line)
+            # value = re.findall('-?[1-9]+[0-9]*.?[0-9]*E-?\+?[0-9]+', line)
+            # value = re.findall('\s-?[1-9]+[0-9]*.?[0-9]*E-?\+?[0-9]+\s?', line)
+            # print line
+            return value[0]
