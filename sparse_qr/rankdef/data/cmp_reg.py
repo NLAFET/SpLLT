c495c973a00650ebb4a32e6aef2c1cb3ea9163c6
JohnConnor <florent.joan.lopez@gmail.com>
2016-07-07 15:01:25 +0100

    added script

--- sparse_qr/rankdef/data/cmp_reg.py
+++ sparse_qr/rankdef/data/cmp_reg.py
@@ -1,11 +1,35 @@
 #!/usr/bin/env python
 
-import ValueExtractor as ve
+import sys
+import os
+import fileinput
+import re
 
-sigma = [-6, -12]
+import vextractor as ve
 
-for mat in open(list.matrix):
+sigmas = [-6, -12]
+
+flop_str     = 'Estimated total flops at facto               :'
+flop_reg_str = 'Estimated total flops at facto (regularized) :'
+
+for mat in open('list.matrix'):
+
+    mat = mat.rstrip()
+    pbl = re.sub(r'.mtx', '', mat)
+    print pbl
     
-    flops = ve.SingleFloat(r'Estimated total flops at facto               :')
+    for sigma in sigmas:
 
-    print flops
+        # print sigma
+
+        datafile = pbl + '_SIGMA-1e' + str(sigma)
+        f = open(datafile)
+        # fl = ve.get_value(f, flop_str)
+        # f.seek(0)
+        flr = ve.get_value(f, flop_reg_str)
+        f.close()
+                    
+        print flr
+        
+        # flops = ve.SingleFloat(r'Estimated total flops at facto               :')
+        # print flops
