4181dd75a3fbce5d5333d5d79199df7f88915ea2
Vedran Novakovic <vedran.novakovic@stfc.ac.uk>
2016-12-13 17:41:55 +0000

    MKL tester script renamed and expanded.

--- bench_kernels/bench_potrf/bench_MKL_dpotrf.sh
+++ bench_kernels/bench_potrf/bench_MKL_dpotrf.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+export DATA_DIR=../../../ralna/TR-SpLLT_GPU/data/`hostname`
+echo "DATA_DIR=$DATA_DIR"
 #export OMP_PLACES="0,2,4,6,8,10,12,14,16,18,20,22,24,26,28,30" # CPU0
 #export OMP_PLACES="0,2,4,6,8,10,12,14" # CPU0,THR0
 export OMP_PLACES="{16,18,20,22,24,26,28,30}" # CPU0,THR1
@@ -9,5 +11,10 @@ for ((I=1; I<=8; ++I))
 do
 	export MKL_NUM_THREADS=$I
 	echo "MKL_NUM_THREADS=$MKL_NUM_THREADS"
-	/usr/bin/numactl -m 0 -N 0 ./bench_MKL_potrf.exe $1 $2 $3 $4 > MKL_potrf-$I-$1-$2-$3-$4-`date +%s`.csv
+	export OUT_CSV=MKL_dpotrf-$I-$1-$2-$3-$4-`date +%s`.csv
+	echo "OUT_CSV=$OUT_CSV"
+	/usr/bin/numactl -m 0 -N 0 ./bench_MKL_potrf.exe $1 $2 $3 $4 > $DATA_DIR/$OUT_CSV
+	pushd $DATA_DIR
+	git add -v $OUT_CSV
+	popd
 done
