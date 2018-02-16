cb71dce1ccce157667ccf89f9552fd080eb3390b
cayrols <sebastien.cayrols@stfc.ac.uk>
2018-02-16 14:17:11 +0000

    Change get_wtime into omp_get_wtime, a binding of the omp routine; add a fortran function to return the time as a real

--- src/get_wtime_mod.F90
+++ src/get_wtime_mod.F90
@@ -2,10 +2,19 @@ module get_wtime_mod
   implicit none
 
   interface
-     function get_wtime() bind(C)
+     function omp_get_wtime() bind(C)
        use iso_c_binding
-       real(c_double) :: get_wtime 
-     end function get_wtime
+       real(c_double) :: omp_get_wtime 
+     end function omp_get_wtime
   end interface
+
+contains
+
+! function get_fwtime()
+!   real :: get_fwtime
+!   character(len=10) :: time
+!   call date_and_time(TIME=time)
+!   read(time, *) get_fwtime
+! end function get_fwtime
 
 end module get_wtime_mod
